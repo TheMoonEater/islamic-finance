0454f73999d23ebdfc7c467896d9e8397627b0c9
TheMoonEater <amani.laichi2@gmail,com>
2026-06-27 18:34:57 +0100

    document a ajouter dans mon dossier

--- documents/serializers.py
+++ documents/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-
 from .models import Document
 
 
-class DocumentSerializer(
-    serializers.ModelSerializer
-):
+class DocumentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Document
         fields = "__all__"
+        read_only_fields = ["user", "valide", "created_at"]
